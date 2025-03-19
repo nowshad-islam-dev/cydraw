d7af983b52bce95f0c10fb7e472e371f13e87ed5
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-03-20 01:46:55 +0600

    run first dynamic render of whiteboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Stage, Layer, Circle } from 'react-konva';
 
 const App = () => {
-  return <div>hello from frontend cydraw</div>;
+  const [shapes, setShapes] = useState([]);
+
+  const handleStageClick = (e) => {
+    const { x, y } = e.target.getStage().getPointerPosition();
+    setShapes([...shapes, { type: 'circle', x, y }]);
+  };
+  return (
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onClick={handleStageClick}
+    >
+      <Layer>
+        {shapes.map(
+          (shape, index) =>
+            shape.type === 'circle' && (
+              <Circle
+                key={index}
+                x={shape.x}
+                y={shape.y}
+                radius={30}
+                fill="green"
+              />
+            )
+        )}
+      </Layer>
+    </Stage>
+  );
 };
 
 export default App;
