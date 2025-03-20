ffaa6001d83e173e73ad1361af108c7d2eb60c9d
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-03-20 16:40:54 +0600

    update component to use state from store

--- frontend/src/components/Toolbar.jsx
+++ frontend/src/components/Toolbar.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import { Button, Toolbar, AppBar } from '@mui/material';
+import useStore from '../store';
 
-const ToolbarComponent = ({ onToolSelect }) => {
+const ToolbarComponent = () => {
+  const setTool = useStore((state) => state.setTool);
   return (
     <AppBar position="static">
       <Toolbar className="flex justify-between">
         <Button
           variant="contained"
           color="primary"
-          onClick={() => onToolSelect('circle')}
+          onClick={() => setTool('circle')}
         >
           Circle
         </Button>
         <Button
           variant="outlined"
           color="secondary"
-          onClick={() => onToolSelect('rect')}
+          onClick={() => setTool('rect')}
         >
           Rectangle
         </Button>
-        <Button
-          variant="text"
-          color="inherit"
-          onClick={() => onToolSelect('line')}
-        >
+        <Button variant="text" color="inherit" onClick={() => setTool('line')}>
           Line
         </Button>
       </Toolbar>
